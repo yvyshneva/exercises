a3644ba9c530e57da7b3e68043fefd90cec701ab
Yana <26516239+yvyshneva@users.noreply.github.com>
2022-09-05 14:04:32 +0200

    Update code_snippets.py

--- code_snippets.py
+++ code_snippets.py
@@ -37,3 +37,53 @@ int_1 = int(input())
 int_2 = int(input())
 
 print(int_1 + int_2)
+
+
+############## FizzBuzz #################
+"""
+FizzBuzz is a well known programming assignment, asked during interviews.
+
+The given code solves the FizzBuzz problem and uses the words "Solo" and "Learn" instead of "Fizz" and "Buzz".
+It takes an input n and outputs the numbers from 1 to n.
+For each multiple of 3, print "Solo" instead of the number.
+For each multiple of 5, prints "Learn" instead of the number.
+For numbers which are multiples of both 3 and 5, output "SoloLearn".
+
+You need to change the code to skip the even numbers, so that the logic only applies to odd numbers in the range.
+"""
+
+n = int(input())
+
+for x in range(1, n, 2):
+    if x % 3 == 0:
+        if x % 5 == 0:
+            print("SoloLearn")
+        else:
+            print("Solo")
+    elif x % 5 == 0:
+        print("Learn")
+    else:
+        print(x)
+
+
+############## Celsius to Fahrenheit #################
+"""
+You are making a Celsius to Fahrenheit converter.
+Write a function to take the Celsius value as an argument and return the corresponding Fahrenheit value.
+
+Sample Input
+36
+
+Sample Output
+96.8
+"""
+
+celsius = int(input())
+
+def conv(c):
+    return 9/5 * c + 32
+    
+
+fahrenheit = conv(celsius)
+print(fahrenheit)
+
