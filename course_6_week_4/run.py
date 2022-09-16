43439059eb1fbe869e28df0409477a819da5d7df
Yana <26516239+yvyshneva@users.noreply.github.com>
2022-09-16 12:47:54 +0200

    Create run.py

---
+++ course_6_week_4/run.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+import os
+import requests
+
+url = "http://localhost/fruits/"
+
+dir_txt = "supplier-data/descriptions/"
+description_files = os.listdir(dir_txt)
+
+fields = ['name', 'weight', 'description']
+for file in description_files:
+    with open(dir_txt + file, "r") as f:
+        text = f.read().split('\n')
+        image_data = dict(zip(fields, text))
+
+        image_data['weight'] = int(image_data['weight'][:-4])
+        image_data['image_name'] = file.replace(".txt", ".jpeg")
+
+        response = requests.post(url, data=image_data)
+        if response.status_code == 201:
+            print("Request was succeeded")
+        else:
+            print("Response: ", response.status_code)
