96066ed052e117aedd35bcf00d9e6599e741fda9
Yana <26516239+yvyshneva@users.noreply.github.com>
2022-09-16 14:03:40 +0200

    Create report_email_new.py

---
+++ course_6_week_4/report_email_new.py
@@ -0,0 +1,36 @@
+
+#!/usr/bin/env python3
+
+import datetime
+import os
+import reports
+import emails
+
+
+def process_data(directory):
+    files = os.listdir(directory)
+
+    all_data = {}
+    for file in files:
+        with open(directory + file, "r") as f:
+           text = f.read().split("\n")[:2]
+           data = {'name': text[0],
+                   'weight': text[1],
+                   "\n":""}
+           all_data.update(data)
+    return all_data
+
+today = datetime.date.today()
+
+if __name__ == "__main__":
+    data = process_data("supplier-data/descriptions/")
+    title = f"Processed Update on {today}"
+    reports.generate_report('/tmp/processed.pdf', title, data)
+
+    sender = "automation@example.com"
+    receiver = "{}@example.com".format(os.environ.get('USER'))
+    subject = "Upload Completed - Online Fruit Store"
+    body = "All fruits are uploaded to our website successfully. A detailed list is attach$
+
+    message = emails.generate_email(sender, receiver, subject, body, "/tmp/processed.pdf")
+    emails.send_email(message)
